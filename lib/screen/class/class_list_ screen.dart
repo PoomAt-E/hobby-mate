060a48dc343569a7374a8e8ba2e086b3c43be50e
hyejinxx <haejinjung1110@gmail.com>
2023-04-17 20:23:33 +0900

    feat: 옵션(카테고리) 추가

--- lib/screen/class/class_list_ screen.dart
+++ lib/screen/class/class_list_ screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hobby_mate/widget/appbar.dart';
 import 'package:hobby_mate/widget/home/class_box.dart';
 
 class ClassListScreen extends StatefulWidget {
@@ -14,27 +14,99 @@ class _ClassListScreenState extends State<ClassListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(slivers: [
-      SliverAppBar(
+        appBar: const MainAppbar(),
         backgroundColor: Colors.white,
-        expandedHeight: 100,
-        flexibleSpace: FlexibleSpaceBar(
-          title: Text('오늘의 추천 강좌',
-              style: TextStyle(color: Colors.black, fontSize: 16)),
-          background: Container(
-            color: Colors.amber,
-          ),
-        ),
-      ),
-      SliverFillRemaining(
-          child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Flex(direction: Axis.vertical, children: [
-                Expanded(
-                    child: Column(
-                        children:
-                            [1, 2, 3].map((e) => ClassBoxWidget()).toList()))
-              ])))
-    ]));
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              optionButton(),
+              Expanded(
+                  child: Column(
+                      children: [1, 2, 3]
+                          .map((e) => const ClassBoxWidget())
+                          .toList()))
+            ])));
+  }
+
+  Widget optionButton() {
+    return TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                    color: Color.fromARGB(44, 0, 0, 0), width: 1),
+                borderRadius: BorderRadius.circular(5.0))),
+        onPressed: () => optionSheet(),
+        child: Row(mainAxisSize: MainAxisSize.min, children: const [
+          Text('필터', style: TextStyle(color: Colors.black87)),
+          Icon(
+            Icons.filter_alt_rounded,
+            color: Colors.black54,
+            size: 15,
+          )
+        ]));
+  }
+
+  void optionSheet() {
+    Future<void> a = showModalBottomSheet<void>(
+        context: context,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0))),
+        builder: (BuildContext context) => const OptionBottomSheet());
+  }
+}
+
+class OptionBottomSheet extends ConsumerWidget {
+  const OptionBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final list = ['전체보기', 'Music', 'Movie', 'Game', 'Book', 'Sport', 'Etc'];
+    return SizedBox(
+        height: MediaQuery.of(context).size.height * 0.6,
+        child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: list.length,
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            itemBuilder: (context, index) {
+              return InkWell(
+                  onTap: () {
+                    // ref
+                    //     .read(counselorListProvider.notifier)
+                    //     .addOption('type', list[index]);
+
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                      margin: index != 0
+                          ? const EdgeInsets.symmetric(vertical: 20)
+                          : const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            list[index],
+                            style: index == 0
+                                ? TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.red[600])
+                                : const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black),
+                          ),
+                          const Icon(
+                            Icons.arrow_forward_ios,
+                            size: 15,
+                          ),
+                        ],
+                      )));
+            }));
   }
 }
